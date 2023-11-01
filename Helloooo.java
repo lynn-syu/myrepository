a0371eb5c312e057b429056dbeb527eed6e8c5a5
lynn <mllynn1001@gmail.com>
2023-11-01 15:01:54 +0800

    update

--- Helloooo.java
+++
@@ -1,9 +0,0 @@
-public class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("Hi,there");
-		System.out.println("conflict1");
-		System.out.printLn("conflict2");
-    }
-
-// test
-}
