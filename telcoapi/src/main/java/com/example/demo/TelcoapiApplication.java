55d796e9470260f4a43a2b67139ba61549c031e0
lynn <mllynn1001@gmail.com>
2023-11-01 14:56:15 +0800

    init

---
+++ telcoapi/src/main/java/com/example/demo/TelcoapiApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelcoapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TelcoapiApplication.class, args);
+	}
+
+}
